45255c5250c5fc7f428fbd97df8f305ffebb7a52
Luis A. Nunes Amaral [amaral@northwestern.edu] <amaral@northwestern.edu>
2022-12-26 16:03:33 -0600

    correction with errors in previous modules and addition of notebooks to data analysis module
    correction with errors in previous modules and addition of notebooks to data analysis module

--- Module_Natural_Language_Processing/my_nlp_library.py
+++ Module_Natural_Language_Processing/my_nlp_library.py
@@ -1,7 +1,7 @@
 __author__ = "Amaral LAN"
 __copyright__ = "Copyright 2022, Amaral LAN"
 __credits__ = ["Amaral LAN"]
-__version__ = "0.1"
+__version__ = "0.2"
 __maintainer__ = "Amaral LAN"
 __email__ = "amaral@northwestern.edu"
 __status__ = "beta"
@@ -15,60 +15,63 @@ from random import random
 from string import punctuation, whitespace
 
 
-def read_text_file(file_path, encoding = 'UTF-8'):
+def read_complete_works():
     """
-    This function takes a path to a text file with a specific
-    encoding and returns a list of lines in the file.
+    This function reads the file with Complete Works of William Shakespeare
+    and returns a list with lines in the file and a dictionary information about 
+    each of the works included.
     
     inputs:
-        file_path -- str or PosixPath object
-        encoding -- str
+        None
         
     returns:
-        all_the_lines -- list of str
-    """
-    with open(file_path, 'r', encoding= encoding) as file_in:
-        all_the_lines = file_in.readlines()
-        
-    return all_the_lines
-
-
-def extract_play(play_name, complete_works):
-    """
-    This function takes as input the name of a play in 
-    Shakespeare's Complete Works text file from Project
-    Gutenberg and returns the lines for that play
-    
-    inputs:
-        play_name -- str
         complete_works -- list of str
+        plays -- dict with play title as key and [year, start_line, end_line] as value 
         
-    returns:
-        the_play -- list of str
     """
-    # find line_start and line_end
-    #
-    found_start = False
-    for i, line in enumerate(complete_works):
-        if line.strip() == play_name:
-            print(f"{i:>4} -- {line.strip()}")
-            line_start = i
-            found_start = True
-            continue
-        
-        if found_start and line.strip() == 'THE END':
-            print(f"{i:>4} -- {line.strip()}")
-            line_end = i
-            break
-        
-    the_play = complete_works[line_start:line_end]
-
-    print(f"\nThe play {play_name} has {len(the_play)} lines.\n")
+    file_path = Path.cwd() / 'Data' / 'Shakespeare.txt'
+    with open( file_path, 'r', encoding = 'UTF-8' ) as file_in:
+        complete_works = file_in.readlines()
     
-    return the_play
+    elect_pattern = '<<THIS ELECTRONIC VERSION OF THE COMPLETE WORKS OF WILLIAM'
+    inside_flag = False
+    found_play = False
+
+    plays = {}
+    for i, line in enumerate(complete_works):
+        if not inside_flag and line.strip() == elect_pattern:
+            inside_flag = True
+            continue
+
+        if inside_flag and line.strip().isnumeric():
+            if ( complete_works[i+2].upper() == complete_works[i+2] or
+                 complete_works[i+3].upper() == complete_works[i+3] or 
+                 complete_works[i+6].upper() == complete_works[i+6]):
+                
+                found_play = True
+                year_play = int( line.strip() )
+
+                if complete_works[i+2].strip() == '':
+                    if complete_works[i+3].strip() == '':
+                        title_play = complete_works[i+6].strip()
+                        plays[title_play] = [year_play, i+6]
+                    else:
+                        title_play = complete_works[i+3].strip()
+                        plays[title_play] = [year_play, i+3]
+                else:    
+                    title_play = complete_works[i+2].strip()
+                    plays[title_play] = [year_play, i+2]
+                continue
+            
+        if found_play and line.strip() == 'THE END':
+            plays[title_play].append(i)
+            found_play = False
+            inside_flag = False
+
+    return complete_works, plays
 
 
-def get_characters(the_play, boundary_line = 25):
+def get_characters(the_play):
     """
     This function takes as input a list of lines from
     a play in Shakespeare's Complete Works text file 
@@ -78,36 +81,50 @@ def get_characters(the_play, boundary_line = 25):
     
     inputs:
         play_name -- str
-        boundary_line -- int defining the range of lines
         
     returns:
-        personae -- dict with character names as keys
-                    and descriptions as value
+        personae -- dict with character name as key
+                    and character description as value
     """
+    # Get relevant lines
+    #
+    start_pattern = 'Dramatis Personae'
+    end_pattern = '<<THIS ELECTRONIC VERSION OF THE COMPLETE WORKS OF WILLIAM'
+
     found_personae = False
-    personae = {}
     for i, line in enumerate(the_play):
-        if line.strip() == 'Dramatis Personae':
-            print(f"{i:>4} -- {line.strip()}")
-            line_start = i
-            found_personae = True
-            continue
+        if start_pattern in line:
+            start_line = i + 1
+            continue 
+
+        if end_pattern in line:
+            end_line = i
+            break
+
+    personae_text = the_play[start_line: end_line]
+    print( personae_text )
+    print()
     
-        if found_personae and i < boundary_line:
-            words = line.strip().split(',')
+    
+    personae = {}  
 
-            # key is first item
-            key = words[0].strip()
+    for i, line in enumerate(personae_text):
+        if line == '\n':
+            continue
 
-            # empty or not in upper letters is not character
-            if not key or key.upper() != key:
-                print(f"{i:>4} --{key}-- Not a character.")
-                continue
-        
-            # value is the joint with commas of following items
-            value = ','.join(words[1:])        
-            print(f"{i:>4} -- {key:>20} -- {value}")
-            personae[key] = value
+#         print(f"{i:>3} -- {line.strip()}")
+
+        # Split by comma, take first item if is all caps, 
+        # join the rest
+        #
+        items = line.strip().split(',')
+
+        if items[0].upper() == items[0]:
+            name = items[0].strip()
+            description = ','.join(items[1:]).strip()
+            personae[name] = description
+
+    print( personae )
 
     return personae
 
@@ -127,24 +144,23 @@ def get_character_lines(character, the_play):
     """
     character_lines = []
     character = character.upper()
-    start_string = '  ' + character
-    continuation_string = '    '
+    
+    start_string = ' '*2 + character
+    continuation_string = ' '*4
     
     found_lines = False
     for i, line in enumerate(the_play):
         if line.split('.')[0] == start_string:
-#             print(f"{i:>4}--{line.rstrip()}")
             found_lines = True
-            character_lines.append(line)
+            character_lines.append(line.replace(start_string+ '.', '').strip())
             continue
 
         if found_lines:
-            if line[:4] == continuation_string:
-#                 print(f"{i:>4}--{line.rstrip()}")
-                character_lines.append(line)
+            if line[:len(continuation_string)] == continuation_string:
+                character_lines.append(line.strip())
             else:
                 found_lines = False
-                continue    
+                continue 
     
     return character_lines
 
@@ -171,10 +187,8 @@ def extract_words_from_lines(character, character_lines):
             line = line.replace(character_string, ' ')
 
         line_words = line.strip().split()
-#         print(f"{i:>4} -- {line_words}")
 
         for word in line_words:
-#             print(f"\t{word.rstrip(punctuation)}")
             character_words.append(word.rstrip(punctuation).lower())
 
     print(f"{character.capitalize()}'s lines comprise {len(character_words)} words.\n")
@@ -220,26 +234,3 @@ def plot_survival_function_word_frequency(corpus_name, counter, ax):
     return
 
 
-def text_entropy(counter):
-    """
-    This function takes a Counter object and returns the entropy of the 
-    underlying text.
-    
-    inputs:
-        counter -- Counter object
-        
-    returns:
-        entropy -- float
-    """
-    entropy = 0
-    
-    counts = list( dict(counter).values() )
-    N = sum(counts)
-    
-    for n_i in counts:
-        p_i = n_i / N
-        entropy -= p_i * np.log2(p_i)
-
-    print(f"The entropy of this corpus is {entropy:.2f}\n" )
-    
-    return entropy
